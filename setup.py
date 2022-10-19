58fee7a92ec5d6476416d7ab1c0f2ceea53925cb
Vincent Schmandt <vincentscode@gmail.com>
2022-10-19 12:08:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='obsidian-html',
     version='0.1',
     description='Converts an Obsidian vault into HTML',
-    url='https://github.com/kmaasrud/obsidian-hugo',
-    author='kmaasrud',
-    author_email='kmaasrud@outlook.com',
+    url='https://github.com/vincentscode/obsidian-html',
+    author='Vincent Schmandt',
+    author_email='vincentscode@gmail.com',
     license='MIT',
     packages=['obsidian_html'],
     install_requires=[
